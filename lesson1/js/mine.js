693edb863e86ddfdae15d81f2203bd414e45544e
Vlad <effectcor@gmail.com>
2019-02-28 23:05:05 +0300

    created function getDate and object appData

--- lesson1/js/mine.js
+++ lesson1/js/mine.js
@@ -1,4 +1,19 @@
 let money 
 let date 
 
+appData = { 
+    budget: money, 
+    timeData: getDate(),
+    expenses: {},
+    optionlExpenses: {},
+    income: [],
+    saving: false,
+}
+
+
+
+function getDate() { 
+    return new Date().toLocaleDateString()
+}
+
 
