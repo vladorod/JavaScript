5ce36823250f8456f444b5396f80b23bc4d80af7
Vlad <effectcor@gmail.com>
2019-03-01 07:23:00 +0300

    added additional lesson

---
+++ lesson1/additionalLesson/js/mine.js
@@ -0,0 +1,19 @@
+
+math = {
+    multiplay: (num) => { 
+        var sum = Number(num[0])
+        for (let i = 0; i < num.length; i+1) {
+            j = i++
+            console.log(`оборот ${i} ` + 'сумма: ' + sum, 'умножение: ' + sum + '*' +  num[j+1])
+            console.log(sum = sum * Number(num[j+1]));            
+}
+}, 
+    plus: (num) => { 
+            var sum = 3
+            for (let i = 0; i < num.length; i+1) {
+                j = i++
+                console.log(`оборот ${i} ` + 'сумма: ' + sum, 'сложение: ' + sum + '+' +  num[j+1])
+                console.log(sum = sum + Number(num[j+1]));            
+    }
+}}
+math.multiplay("33721")
