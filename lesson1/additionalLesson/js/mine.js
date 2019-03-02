c19ac732858de9b5fe00565d0901875956adcbb1
Vlad <effectcor@gmail.com>
2019-03-02 23:33:06 +0300

    added addititonal lesson 1

--- lesson1/additionalLesson/js/mine.js
+++ lesson1/additionalLesson/js/mine.js
@@ -5,8 +5,9 @@ math = {
         var sum = Number(num[0])
         for (let i = 0; i < num.length; i+1) {
             j = i++
-            console.log(`оборот ${i} ` + 'сумма: ' + sum, 'умножение: ' + sum + '*' +  num[j+1])
-            console.log(sum = sum * Number(num[j+1]));            
+            if (j > num.length-2) { break; } else { console.log(`оборот ${i} ` + 'сумма: ' + sum, 'умножение: ' + sum + '*' +  num[j+1])
+            console.log(sum = sum * Number(num[j+1])); }
+                   
 }
 }, 
     plus: (num) => { 
@@ -14,9 +15,10 @@ math = {
             
             for (let i = 0 ; i < num.length; i+1) {
                 j = i++;
-                if ( j  < num.length ) { break; } else { console.log(`оборот ${i} ` + 'сумма: ' + sum, 'сложение: ' + sum + '+' +  num[j+1])
+                if ( i > num.length-1 ) { break; } else { console.log(`оборот ${i} ` + 'сумма: ' + sum, 'сложение: ' + sum + '+' +  num[j+1])
                 console.log(sum = sum + Number(num[j+1])); }
                          
     }
 }}
+math.multiplay("33721") 
 math.plus("33721") 
