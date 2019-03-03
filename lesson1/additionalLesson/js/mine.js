8e43a63c1df3540c321242a2f710bd4861f741da
Vlad <effectcor@gmail.com>
2019-03-04 01:02:26 +0300

    ADDEDMULTIPLAY LESSON 1 AND CONVAS

--- lesson1/additionalLesson/js/mine.js
+++ lesson1/additionalLesson/js/mine.js
@@ -1,24 +1,20 @@
-'use script'
+'use scriсt'
 
-math = {
-    multiplay: (num) => { 
+let param = '33721'
+
+function multiplay (num)  { 
         var sum = Number(num[0])
-        for (let i = 0; i < num.length; i+1) {
+        for (let i = 0; i < num.length; i+1) 
+        {
             j = i++
-            if (j > num.length-2) { break; } else { console.log(`оборот ${i} ` + 'сумма: ' + sum, 'умножение: ' + sum + '*' +  num[j+1])
-            console.log(sum = sum * Number(num[j+1])); }
-                   
+            if (j > num.length-2) { break; } else 
+            {  
+            sum = sum * Number(num[j+1]) 
+            }
+        }
+  return sum 
 }
-}, 
-    plus: (num) => { 
-            var sum = Number(num[0])
-            
-            for (let i = 0 ; i < num.length; i+1) {
-                j = i++;
-                if ( i > num.length-1 ) { break; } else { console.log(`оборот ${i} ` + 'сумма: ' + sum, 'сложение: ' + sum + '+' +  num[j+1])
-                console.log(sum = sum + Number(num[j+1])); }
-                         
-    }
-}}
-math.multiplay("33721") 
-math.plus("33721") 
+
+let mult =  String(multiplay('33721') ** 3) 
+
+console.log(`Умножение - ${mult[0]}${mult[1]}`)
