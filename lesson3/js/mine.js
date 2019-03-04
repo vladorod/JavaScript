eea1206dfdc0cc5f92f2e008a633783e09e0d727
Vlad <effectcor@gmail.com>
2019-03-05 00:52:35 +0300

    edit

--- lesson3/js/mine.js
+++ lesson3/js/mine.js
@@ -40,7 +40,7 @@ function chooseExpenses() {
    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
        b = prompt(`Во сколько обойдется?`, '');
 
-   if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(a)) != null && a != '' && b!='' && a.length < 50 ) 
+   if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b!='' && a.length < 50 ) 
    {
       console.log('done');
 
