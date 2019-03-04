9ffa87c504b4dffec01e2cc491a967db33b613ac
Vlad <effectcor@gmail.com>
2019-03-04 23:04:56 +0300

    added additionalLesson lesson 3

--- lesson3/additionalLesson/js/mine.js
+++ lesson3/additionalLesson/js/mine.js
@@ -1,64 +1,67 @@
 'use strict'
 
-let week = ['понедельник','вторник','среда','четрвег', 'пятница','суббота','воскресенье']; 
-let root = document.querySelector('#root')
-let i = 0
+let str = 'урок-3-был слишком легким';
 
-for(let i in week) { 
-    if (week[i] == day()) { root.insertAdjacentHTML('afterbegin', `<span style="font-style: italic;"> > ${ week[i] } </span> <br>`); continue;}
-   switch(week[i]) 
-    {
-        case 'суббота' :
-        root.insertAdjacentHTML('afterbegin', `<span style="font-weight: bolder"> ${ week[i] } </span> <br>`);
-        continue;
-        case 'воскресенье' :
-        root.insertAdjacentHTML('afterbegin', `<span style="font-weight: bolder"> ${ week[i] } </span> <br>`);
-        continue;
-        default: root.insertAdjacentHTML('afterbegin', `<span > ${ week[i] } </span> <br>`);
+ 
+// 1
 
-    }
-    
+str = str[0].toUpperCase() + str.substr(1, str.length);
+str = str.split('');
+// 2
+
+for (let i = 0; i < str.length; i++) { 
+    if (str[i] == " ") { 
+        str.splice(i, 1, '-' );
+    }; 
+};
+str = str.join('');
+console.log(str);
+
+//3 
+
+str = str.split('-');
+let o = str[str.length - 1].split('');
+o.splice(o.length-2 , 2,"о","о");
+o = o.join('');
+str.splice(str.length - 1, 1, o);
+str = str.join('-');
+
+console.log(o);
+console.log(str);
+
+//4 
+
+let arr = [20, 33, 1, 'Человек', 2, 3];
+
+arr.splice(3,1);
+let sum = [];
+for (let i = 0; i < arr.length; i++) {
+    if (i + 1 < arr.length) { 
+    sum.push(arr[i]**3 + arr[i + 1]**3)
+}else break;
 }
-
-
-function day() 
-{
-    let week = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота']; 
-    let date = new Date
-    let datetoday = date.getDay()
-    return  week[datetoday]
+let end = 0;
+for (let i = 0; i < sum.length; i++) {
+    end = end + sum[i];
 }
+console.log(end);
+console.log(Math.sqrt(end));
 
+// 4 
 
+function delSpace (space) { 
+if ((typeof(space)) != 'string') { 
+    console.log('not string')
+}
+space = space.split('');
+if ( space[0] == ' ' ) {
+    delete space[0]
+  } 
 
-// 2) Создать массив arr = []
+if ( space[space.length - 1] == ' ' ) {
+    console.log('work')
+    delete space[space.length -1 ]
+  }
+ return space.join('')
 
-
-let arr = ['137343','214848','7421', '5665345','365487']
-
-
-    let pass = []
-    for (let i = 0; i < arr.length; i++) { 
-        let array = arr[i]
-        for (let j = 0; j < array.length; j++) {
-            if (array[0] == '3'|| array[0] == '7' ) 
-            { 
-                pass.push(array)
-                break;  
-            }
-        }
-    }
-    console.log(pass)
- 
- 
-
- 
- 
-
-function serch( where, what ) { 
-    for ( i in where ) { 
-      if ( where[0] == what ) { 
-          return where[i]  
-         }  else return false 
-    }
 }
