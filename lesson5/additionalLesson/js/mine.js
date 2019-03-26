4ab3b88844f689f1ce7fe147d8997daa814b7dfc
Vlad <effectcor@gmail.com>
2019-03-27 01:52:51 +0300

    added lesson 5 edit

--- lesson5/additionalLesson/js/mine.js
+++ lesson5/additionalLesson/js/mine.js
@@ -1,56 +1,44 @@
-
 'use scrict'
 
 let root = document.querySelector('#root'),
-    passDate = document.querySelector('#passDate'),
-    d = new Date(),
-    h = addZero(d.getHours()),
-    m = addZero(d.getMinutes()),
-    s = addZero(d.getSeconds()),
-    day = addZero(d.getDay()),
-    mon = addZero(d.getMonth()),
-    yea = d.getFullYear();
+  passDate = document.querySelector('#passDate'),
+  d = new Date(),
+  h = addZero(d.getHours()),
+  m = addZero(d.getMinutes()),
+  s = addZero(d.getSeconds()),
+  day = addZero(d.getDay()),
+  mon = addZero(d.getMonth()),
+  yea = d.getFullYear(),
+  input1 = document.querySelectorAll('input')[0],
+  input2 = document.querySelectorAll('input')[1],
+  eq = document.querySelectorAll('input')[2];
 
 root.innerHTML = `<h1>${h}:${m}:${s} ${day}:${mon}:${yea} > ${Day()} </h1>`;
 
 function Day() {
-    let week = ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота']; 
-    let date = new Date;
-    let datetoday = date.getDay();
-    return  week[datetoday];
+  let week = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+  let date = new Date;
+  let datetoday = date.getDay();
+  return week[datetoday];
 }
 
 function addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
+  if (i < 10) {
+    i = "0" + i;
   }
-  
-  
+  return i;
+}
 
-  function getDaysLeft(oDeadLineDate, d){
-    return oDeadLineDate > d ? Math.ceil((oDeadLineDate - d) / (1000 * 60 * 60 * 24)) : null;
-  }
-  
-function MkDaysLeft(sDeadLineDate){
+document.querySelector('button').addEventListener('click', () => { 
+  eq.value = calcDate(input1.value, input2.value);
+})
 
-    console.log(sDeadLineDate)
-    let sTime = sDeadLineDate + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    let oDeadLineDate = new Date(sTime);
-    let nDaysLeft = getDaysLeft(oDeadLineDate, d);
-    
-    if (nDaysLeft){
-      let sDaysLeft = String(nDaysLeft);
-      let sDaysText = "дней";
-      let nDaysLeftLength = sDaysLeft.length;
-      
-      if (sDaysLeft.charAt(nDaysLeftLength - 2) != "1") {
-        if (sDaysLeft.charAt(nDaysLeftLength - 1) == "2" || sDaysLeft.charAt(nDaysLeftLength - 1) == "3" || sDaysLeft.charAt(nDaysLeftLength - 1) == "4") sDaysText = "дня";
-        else if (sDaysLeft.charAt(nDaysLeftLength - 1) == "1") sDaysText = "день";
-      }
-      
-      document.write(nDaysLeft + " " + sDaysText + ".");
-    } 
-      else console.log('Уже прошло')
+function calcDate(first, second) {
+  let firstTime = first.split('-'),
+      firstDate = new Date(`${firstTime[0]}/${firstTime[1]}/${firstTime[2]}`),
+      secondTime = second.split('-'),
+      secondDate = new Date(`${secondTime[0]}/${secondTime[1]}/${secondTime[2]}`);
+  
+  let sec = Math.abs(firstDate.getTime() - secondDate.getTime());
+  return i = Math.ceil(sec/(1000 * 3600 * 24));
 }
